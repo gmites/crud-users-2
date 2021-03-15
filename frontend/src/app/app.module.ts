90162ccbc14898403221a4b0bccb1669006976ee
DESKTOP-HFE9SM7\khaleesi <grace.mites@gmail.com>
2021-03-15 22:58:18 +0100

    Components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { EditUsersComponent } from './components/edit-users/edit-users.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    EditUsersComponent,
+    EditUserComponent,
+    CreateUserComponent
   ],
   imports: [
     BrowserModule,
