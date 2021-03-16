208519935580dadcffa79af50e8492fadb97db2d
DESKTOP-HFE9SM7\khaleesi <grace.mites@gmail.com>
2021-03-16 14:30:28 +0100

    Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component:UsersComponent},
+  {path:'create', component: CreateUserComponent},
+  {path:'edit/:id', component: EditUserComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
-import { EditUsersComponent } from './components/edit-users/edit-users.component';
 import { EditUserComponent } from './components/edit-user/edit-user.component';
 import { CreateUserComponent } from './components/create-user/create-user.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
-    EditUsersComponent,
     EditUserComponent,
-    CreateUserComponent
+    CreateUserComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
